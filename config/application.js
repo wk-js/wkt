9d3e7def94ee61a0eccc9e60ea8a3dfaa628cc8c
Christophe Massolin <christophe.massolin@makemepulse.com>
2017-10-19 11:27:17 +0200

    Clean

--- config/application.js
+++
@@ -1,9 +0,0 @@
-'use strict'
-
-module.exports = function Application() {
-
-  this.configure.add('application:initialize', function() {
-
-  })
-
-}
--- workflow/index.js
+++
@@ -1,8 +0,0 @@
-'use strict'
-
-const Application = require('./lib/application').create()
-global.Application = Application
-
-require(process.cwd() + '/config/application').call(Application)
-
-module.exports = Application
