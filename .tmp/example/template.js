fca6a3587811481bbccc8125bdf3ba16c3ea05cc
Christophe Massolin <christophe.massolin@makemepulse.com>
2017-12-20 17:41:34 +0100

    Update .gitignore

--- .tmp/example/template.js
+++
@@ -1,9 +0,0 @@
-'use strict'
-
-//api( 'file' )
-//api( 'stack' )
-//source( '../example2/template.js' )
-
-after('bundle', function() {
-  remove( 'template.js' )
-})
--- .tmp/example2/example.js
+++
@@ -1,2 +0,0 @@
-console.log('Hello <%= name %> !')
-<%= chunk('some code') %>
